f3b9d9a72d389788a8c247ade650984e200da2fc
Tom Schaul <schaul@gmail.com>
2013-01-24 17:16:32 -0500

    rudimentary MDP conversion works

--- vgdl/mdpmap.py
+++ vgdl/mdpmap.py
@@ -17,6 +17,7 @@ Finally, we produce a set of features/observations. Two possibilities:
   - partially observable: generally from first-person avatar perspective
   
 """
+from scipy import zeros
 from pybrain.utilities import flood
 from ontology import MovingAvatar, BASEDIRS
 from core import VGDLSprite
@@ -29,36 +30,62 @@ class MDPconverter(object):
         
     def __init__(self, game):
         self.game = game
-        obstypes = []
-        allPos = set([(col, row) for row in range(game.height) for col in range(game.width)])    
-        for key, ss in game.sprite_groups.items():
-            print key, len(ss), ss[0]
+        self.actions = BASEDIRS
+        self.sas_tuples = []
+        self.rewards = {}
+        
+    def convert(self, observations=True):
+        if observations:
+            obstypes = []
+            
+        allPos = set([(col, row) for row in range(self.game.height) for col in range(self.game.width)])    
+        for _, ss in self.game.sprite_groups.items():
+            #find avatar
             if isinstance(ss[0], MovingAvatar):
                 self.avatar = ss[0]
-            else:
+            elif observations:
+                # retain observable features
                 tmp = [self.rect2pos(sprite.rect) for sprite in ss if sprite.is_static]
-                print sorted(set(tmp))
                 obstypes.append(tmp)
         initSet = [self.rect2pos(self.avatar.rect)]
-        self.actions = BASEDIRS
-        self.sas_tuples = {}
-        self.rewards = {}
-        print sorted(allPos)
-        print initSet
         self.states = sorted(flood(self.tryMoves, allPos, initSet))
-        print self.states
-        print self.rewards
-        print len(self.sas_tuples)
+        dim = len(self.states)        
+        Ts = [zeros((dim, dim)) for _ in self.actions]
+        R = zeros(dim)
+        for si, pos in enumerate(self.states):
+            if pos in self.rewards:
+                R[si] += self.rewards[pos]
+            for ai, a in enumerate(self.actions):
+                tmp = 0.
+                for di, dest in enumerate(self.states):
+                    if (pos, a, dest) in self.sas_tuples:
+                        Ts[ai][si, di] += 1
+                        tmp += 1                        
+                if tmp > 1:
+                    Ts[a][si, :] /= tmp
+                
+        if observations:
+            # one observation for current position and each of the 4 neighbors.
+            fMap = zeros((len(obstypes)*5, dim))
+            for si, pos in enumerate(self.states):
+                for i, p in enumerate([pos]+self.posNeighbors(pos)):
+                    for j, obs in enumerate(obstypes):
+                        if p in obs:
+                            fMap[j*5+i, si] = 1
+                    
+            return Ts, R, fMap
+        else:
+            return Ts, R
 
-
-    
+    def posNeighbors(self, pos):
+        return [(a[0]+pos[0], a[1]+pos[1]) for a in self.actions]
     
     def rect2pos(self, r):
-        return (r.left/self.game.block_size, r.top/self.game.block_size)
+        return (r.left / self.game.block_size, r.top / self.game.block_size)
     
     def setRectPos(self, r, pos):
-        r.left = pos[0]*self.game.block_size
-        r.top = pos[1]*self.game.block_size
+        r.left = pos[0] * self.game.block_size
+        r.top = pos[1] * self.game.block_size
     
     def tryMoves(self, pos):
         res = []
@@ -74,13 +101,13 @@ class MDPconverter(object):
             # remember the outcome of the action
             dest = self.rect2pos(self.avatar.rect)
             res.append(dest)
-            self.sas_tuples[(pos, a)] = dest            
+            self.sas_tuples.append((pos, a, dest))            
             # remember reward if the final state ends the game
             for t in self.game.terminations[1:]: 
                 # Convention: the first criterion is for keyboard-interrupt termination
                 ended, win = t.isDone(self.game)
                 if ended:
-                    print 'win', win, pos, a, dest
+                    #print 'win', win, pos, a, dest
                     if win:
                         self.rewards[dest] = 1
                     else:
@@ -94,7 +121,13 @@ def testMaze():
     game_str, map_str = maze_game, maze_level_1
     g = VGDLParser().parseGame(game_str)
     g.buildLevel(map_str)
-    MDPconverter(g)
+    C = MDPconverter(g)
+    Ts, R, fMap = C.convert()
+    print C.states
+    print R
+    for T in Ts:
+        print T
+    print fMap
     
 if __name__ == '__main__':
     testMaze()
