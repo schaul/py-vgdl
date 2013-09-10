add2557bd0f21e149bb8ad29619d4ecc016fb160
Tom Schaul <schaul@gmail.com>
2013-09-10 15:47:30 +0100

    crushing boulders

--- examples/gridphysics/boulderdash.py
+++ examples/gridphysics/boulderdash.py
@@ -23,13 +23,14 @@ wwwwwwwwwwwwwwwwwwwwwwwwww
 boulderdash_game = """
 BasicGame
 	SpriteSet
-		avatar > MovingAvatar color=WHITE 
 		dirt > Immovable color=BROWN
 		exitdoor > Immovable color=GREEN
 		diamond > Resource color=YELLOW res_limit=10
 		boulder > Missile orientation=DOWN color=GRAY speed=0.2
-		crab > RandomNPC
-		butterfly > RandomNPC
+		moving > 
+			avatar > MovingAvatar color=WHITE 
+			crab > RandomNPC
+			butterfly > RandomNPC
 	LevelMapping
 		. > dirt
 		E > exitdoor
@@ -39,29 +40,25 @@ BasicGame
 		b > butterfly
 	InteractionSet
 		dirt avatar > killSprite
-		diamond avatar > collectResource
+		diamond avatar > collectResource scoreChange=5
 		diamond avatar > killSprite
-		diamond dirt > stepBack
-		diamond boulder > stepBack
-		diamond wall > stepBack
-		avatar wall > stepBack
-		avatar boulder > stepBack
+		moving wall > stepBack
+		moving boulder > stepBack
+		avatar boulder > killIfFromAbove
+		boulder boulder > stepBack
 		avatar butterfly > killSprite
 		avatar crab > killSprite
 		boulder dirt > stepBack
 		boulder wall > stepBack
 		boulder diamond > stepBack
-		crab wall > stepBack
-		crab boulder > stepBack
 		crab dirt > stepBack
 		crab diamond > stepBack
-		butterfly wall > stepBack
-		butterfly boulder > stepBack
 		butterfly dirt > stepBack
 		butterfly diamond > stepBack
 		crab butterfly > killSprite
-		butterfly crab > transformTo stype=diamond
-		exitdoor avatar > killIfOtherHasMore resource=diamond limit=9
+		butterfly crab > transformTo stype=diamond scoreChange=1
+		exitdoor avatar > killIfOtherHasMore resource=diamond limit=9 scoreChange=100
+	
 	TerminationSet
 		SpriteCounter stype=avatar limit=0 win=False
 		SpriteCounter stype=exitdoor limit=0 win=True
