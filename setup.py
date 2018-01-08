dca7a5f766b7e2fd5cfc346cbc358faafa1ec9f1
martinResearch <martin.delagorce@gmail.com>
2018-01-09 00:51:50 +0100

    add setup.py file
    ading setup.py file to ease the installation of vgdl using easyinstall or pip

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from distutils.extension import Extension
+
+libname="vgdl"
+setup(
+name = libname,
+version="1.0",
+description='A video game description language (VGDL) built on top pf pygame',
+author='Tom Schaul',
+url='https://github.com/schaul/py-vgdl',
+packages=         ['vgdl'],
+install_requires=['pygame']
+)
+
